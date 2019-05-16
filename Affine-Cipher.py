8cf93b475e240dadf8b874b93c1b9cd4fd8cf3dd
Andrew Kevin <49222952+andreweymard@users.noreply.github.com>
2019-05-16 21:59:54 +0530

    Update Affine-Cipher.py

--- Affine-Cipher.py
+++ Affine-Cipher.py
@@ -1,4 +1,4 @@
-from string import ascii_lowercase as alphabet
+from string import ascii_lowercase as alphabet 
 from math import gcd
 
 def encode(plainText, multi, shift):
@@ -10,28 +10,29 @@ def encode(plainText, multi, shift):
 
 def decode(cipherText, multi, shift):
     plainText = ""
-    result = 1
     for i in range(1, len(alphabet)):
         if (multi * i) % len(alphabet) == 1:
-            result = i
+            inv = i
 
     for char in cipherText:
-        plain = ((result * (alphabet.index(char) - shift)) % len(alphabet))
+        plain = ((inv * (alphabet.index(char) - shift)) % len(alphabet))
         plainText += alphabet[plain]
     return plainText
 
-plainText = input("Welcome to my multiffine Cipher Script.\nPlease enter your plain text (Enter only alphabets and spaces):\n")
+plainText = input("Welcome to my Affine Cipher Script.\nPlease enter your plain text.\n"
+                "(Enter only lowercase alphabets, no special characters or spaces.):\n")
 
 while True:
     multi = int(input("Please enter a multiplicative integer:\n"))
     if gcd(multi, len(alphabet)) == 1:
         break
-    print("ValueError: This integer is not a coprime of ", len(alphabet), ".\n")
+    else:
+        print("ValueError: This integer is not a coprime of ", len(alphabet), ".\n")
 
 shift = int(input("Please enter the magnitude of the shift:\n"))
 
 print("This is your cipher text: ", encode(plainText, multi, shift), "\n")
-    
+
 cipherText = encode(plainText, multi, shift)
 
 print("This is your plain text: ", decode(cipherText, multi, shift))
